7183925794eed1eca8746de7ed062e4253d7701e
ViniciusGeraldoZamilian <zamilian.vinicius@escola.pr.gov.br>
2026-06-11 12:42:42 -0300

    lmlk

--- main.js
+++ main.js
@@ -4,34 +4,36 @@ numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
-console.log(botoes)
+console.log(botoes);
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
+
 function diminuiTamanho(){
     if (tamanhoSenha > 1){
-       // tamanhoSenha = tamanhoSenha-1;
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
+
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
-       // tamanhoSenha = tamanhoSenha+1;
        tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
+
 const campoSenha = document.querySelector('#campo-senha');
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
-geraSenha();
 
-campoSenha.value = letrasMaiusculas;
+// Executa a função para gerar a senha inicial
+geraSenha(); 
+
 function geraSenha(){
-    let senha = '' ”;
-    for (let i = 0; i < tamanhoSenha;i++){
-        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+    let senha = ''; // Correção do erro de sintaxe aqui
+    for (let i = 0; i < tamanhoSenha; i++){
+        let numeroAleatorio = Math.random() * letrasMaiusculas.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
         senha = senha + letrasMaiusculas[numeroAleatorio];
     }
