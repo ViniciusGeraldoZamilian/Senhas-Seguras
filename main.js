51439df5615cc752fdd8a2ab9490dda99fd89243
ViniciusGeraldoZamilian <zamilian.vinicius@escola.pr.gov.br>
2026-06-11 08:46:55 -0300

    la ele

--- main.js
+++ main.js
@@ -25,5 +25,15 @@ function aumentaTamanho(){
 }
 const campoSenha = document.querySelector('#campo-senha');
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+geraSenha();
 
 campoSenha.value = letrasMaiusculas;
+function geraSenha(){
+    let senha = '' ”;
+    for (let i = 0; i < tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+}
