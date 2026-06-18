f10cc723466dd4395cc4e1111a7a1047a1e913d4
ViniciusGeraldoZamilian <zamilian.vinicius@escola.pr.gov.br>
2026-06-18 08:44:56 -0300

    ai ai ui ui

--- main.js
+++ main.js
@@ -87,7 +87,7 @@ function classificaSenha(tamanhoAlfabeto){
         forcaSenha.classList.add('fraca');
     }
    const valorEntropia = document.querySelector('.entropia');
-valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
+valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
 }
 
 // Executa a função para gerar a senha inicial depois que tudo foi configurado
